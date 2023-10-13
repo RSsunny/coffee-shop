bc02a623bb7da1da38035fd488ebf80c4d894fef
RSsunny <sunnysharif154@gmail.com>
2023-10-14 00:53:15 +0600

    added navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-
+import logo from "../../assets/logo1 1.png"
+import bgNav from "../../assets/Rectangle 1.png"
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>this is a navbar</h1>
+        <div className="bg-cover bg-center flex justify-center py-2" style={{backgroundImage:`url("${bgNav}")`}}>
+            <div className="flex gap-2 items-center ">
+                <img src={logo} alt="" />
+                <h1 className="text-4xl font-semibold">Espresso Emporium</h1>
+            </div>
         </div>
     );
 };
