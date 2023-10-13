c874cc2ab864a44a6bdb3e79218e0a56327ff6a3
RSsunny <sunnysharif154@gmail.com>
2023-10-14 00:44:27 +0600

    router setup complete

--- src/Router/Rouret.jsx
+++ src/Router/Rouret.jsx
@@ -1,11 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "../Layout/Mainlayout";
+import Error from "../Pages/Error/Error";
+import Home from "../Pages/Home/Home";
 
+const Rouret = createBrowserRouter([
+    {
+        path:'/',
+        element:<Mainlayout></Mainlayout>,
+        errorElement:<Error></Error>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            }
+        ]
+    }
+])
 
-const Rouret = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
 export default Rouret;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import Rouret from './Router/Rouret'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
+    <RouterProvider router={Rouret}></RouterProvider>
   </React.StrictMode>,
 )
